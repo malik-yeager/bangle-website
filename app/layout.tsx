400341d2a60bb9e7006337661e73b7190432f21d
malik-yeager <malik@runnerssoftware.com>
2026-06-30 18:33:15 +0530

    gtag issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Cormorant_Garamond, Jost } from 'next/font/google'
-import Script from 'next/script'
+import { GoogleTagManager } from '@next/third-parties/google'
 import './globals.css'
 
 const cormorant = Cormorant_Garamond({
@@ -68,6 +68,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-56H37SQH" />
       <body className={`${cormorant.variable} ${jost.variable}`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -81,13 +82,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-56H37SQH');`}
-      </Script>
     </html>
   )
 }
